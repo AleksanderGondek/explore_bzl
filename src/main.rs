af2d20d2126852214adfcd8298b64fa3f764d711
Aleksander Gondek <gondekaleksander@protonmail.com>
2026-05-09 19:44:10 +0200

    chore: hardcap number of thread workers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use explore_bzl::explore_bzl;
 
-#[tokio::main]
+#[tokio::main(worker_threads = 4)]
 async fn main() {
   let terminal = ratatui::init();
   let _result = explore_bzl::run(terminal).await;
